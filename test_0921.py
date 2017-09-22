29e1c85517efdbff14709e5a5accc13b329f2fae
sjc7217 <438272461@qq.com>
2017-09-22 11:29:02 +0800

    add windows+pytorch+gpu support

--- test_0921.py
+++ test_0921.py
@@ -1,9 +1,9 @@
 from netCDF4 import Dataset
 
 
-for i in range(30):
+for i in range(300):
 
-    file_RSM_output = "/home/lordshi/PycharmProjects/RSM_PREDICT_NEW/data/validate_input/ACONC.01.lay1.PM2.5." + str(i+403)
+    file_RSM_output = "/home/lordshi/PycharmProjects/RSM_PREDICT_NEW/data/Input/ACONC.01.lay1.PM2.5." + str(i+1)
 
     RSM = Dataset(file_RSM_output, "r", format="NETCDF4")
     print(i)
