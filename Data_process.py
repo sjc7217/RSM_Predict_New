a7040a71b5eafca24e3c79288e71c0f9006c9a04
sjc7217 <438272461@qq.com>
2017-09-24 13:51:08 +0800

    add area 6*5 analysis

--- Data_process.py
+++ Data_process.py
@@ -1,12 +1,44 @@
-import torch
-from torch.autograd import Variable
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import csv
+from netCDF4 import Dataset
 import numpy as np
+import csv
+
+'''
+
+此脚本用于将netCDF文件中的数据提取成按网格点以及训练数据组合的数据
+
+'''
+
+#获取全部训练数据
+paths=["./data/input_new_12/ACONC.01.lay1.PM2.5."+str(i) for i in range(1,369)]
+fin=open("./data/control_0904.csv","r")
+# fout=open("./data/output/")
+reader=csv.reader(fin)
+table=[row for row in reader]
+
+
+#以网格点为单位分别获取各排放因子下的文件数据
+for i in range(0,174):
+    for j in range(0,150):
+        index=0
+        fout=open("./data/output_12_new/out_"+str(i)+'_'+str(j)+".csv","w")
+        writer=csv.writer(fout)
+        for path in paths:
+            res = Dataset(path, "r", format="NETCDF4")
+            num=res.variables["PM25_TOT"][0, 0, i, j]
+            line=table[index]
+            line_new=line.copy()
+            index+=1
+            line_new.append(str(num))
+            #print(type(num))
+
+            #print(line_new)
+            writer.writerow(line_new)
+        fout.close()
+fin.close()
+#     print(rrr.variables["PM25_TOT"][0,0,173,149])
+# rrr.close()
 
 
 
-net = torch.load("./data/net_saved/net_82_94.pkl")
 
-print(net)
+
--- Multiprocessing_with_pool_continue_6_5_new.py
+++ Multiprocessing_with_pool_continue_6_5_new.py
@@ -3,46 +3,46 @@ import torch
 from torch.autograd import Variable
 import csv
 import os
-import numpy as np
 
 '''
-连续化处理整个空间网络的参数拟合
+连续化,区域化处理整个空间网络的参数拟合   ////Windows版本   Linux与Windows上面Python3关于全局变量的变化域似乎有些不同，故此处做一修改
+将整个174*150网格数据切分成6*5小块
+所用神经网络初步定为30*50*30
 
 '''
-
 #训练系数初始值，只需要初始化一次
-para=[]
+para = []
 #神经网络MSE拟合程度度量值
 ACCURACY = 0.5
 #进程数
-PROCESS_NUM = 3
+PROCESS_NUM = 2
 #用于存放训练project的序号
 LIST_NUM = []
 
-#系数和训练参数初始化
-def para_init():
-    para_init_0 = open("./data/output_12_new/out_0_0.csv", "r")
-    reader = csv.reader(para_init_0)
-    for line in reader:
-        para.append([float(i) for i in line[1:-1]])
+para_init_0 = open("../data/output_6_5_new/out_0_0.csv", "r")
+reader = csv.reader(para_init_0)
+for line in reader:
+    para.append([float(i) for i in line[1:31]])
 
-    for x in range(174):
-        for y in range(150):
-            if((not os.path.exists("./data/net_saved_30_40_1_12new/net_" + str(x) + "_" + str(y) + ".pkl"))):
-                LIST_NUM.append([x,y])
-            # PROJECT_LIST.append("./data/output/out_" + str(x) + "_" + str(y) + ".csv")
-            # NET_NAME_LIST.append("./data/net_saved_multi_processing/net_" + str(x) + "_" + str(y) + ".pkl")
-    # print(LIST_NUM)
+#系数和训练参数初始化
+for x in range(29):
+    for y in range(30):
+        if ((not os.path.exists("../data/net_saved_30_40_30_6_5_new/net_" + str(x) + "_" + str(y) + ".pkl"))):
+            LIST_NUM.append([x, y])
+
 
 #训练代码，采用CUDA加速
 def train(net_name,filein):
-    response=[]
 
+    response=[]
+    global para
     #标记值获取并格式化
     reader = csv.reader(filein)
     for line in reader:
-        response.append(float(line[-1]))
+        response.append([float(i) for i in line[31:61]])
+
     factor = torch.FloatTensor(para)
+    #print(factor)
     res = torch.FloatTensor(response)
 
     #cuda()表示此处启用了pytorch的GPU加速
@@ -50,11 +50,11 @@ def train(net_name,filein):
 
     #网格结构定义
     net = torch.nn.Sequential(
-        torch.nn.Linear(30,40),
+        torch.nn.Linear(30,50),
         torch.nn.Sigmoid(),
         # torch.nn.Linear(10,10),
         # torch.nn.Sigmoid(),
-        torch.nn.Linear(40,1)
+        torch.nn.Linear(50,30)
     ).cuda()
 
     #优化器Adagrad
@@ -64,8 +64,9 @@ def train(net_name,filein):
 
     #训练过程，反复调整参数，直到误差loss小于一定值
     while(1):
+        #print(x)
         prediction = net(x).cuda()     # input x and predict based on x
-        loss = loss_func(prediction, y).cuda()     # must be (1. nn output, 2. target)
+        loss = loss_func(prediction, y).cuda()   # must be (1. nn output, 2. target)
         optimizer.zero_grad()   # clear gradients for next train
         loss.backward()         # backpropagation, compute gradients
         optimizer.step()    # apply gradients
@@ -87,25 +88,22 @@ def train(net_name,filein):
 
     print("Saved "+net_name+" successfully!")
 
-
-
 #主调函数，用于不断调用进程池中的任务
 def run_one_time_project(x_y):
     try:
-        f = open("./data/output_12_new/out_" + str(x_y[0]) + "_" + str(x_y[1])  + ".csv", "r")
-        name = "./data/net_saved_30_40_1_12new/net_" + str(x_y[0]) + "_" + str(x_y[1]) + ".pkl"
+        f = open("../data/output_6_5_new/out_" + str(x_y[0]) + "_" + str(x_y[1])  + ".csv", "r")
+        name = "../data/net_saved_30_40_30_6_5_new/net_" + str(x_y[0]) + "_" + str(x_y[1]) + ".pkl"
         #训练开启代码
         train(name, f)
-        f.close()
     except:
         print("Can't open file or reach the bottom!")
-
+    f.close()
 
 #主进程
 if(__name__ == "__main__"):
 
     #LOCK = multiprocessing.Lock()
-    para_init()
+    #para_init()
     #开启进程数量为PROCESS_NUM的进程池
     pool = multiprocessing.Pool(PROCESS_NUM)
     #进程池中的进程依次调用可迭代对象进行计算
